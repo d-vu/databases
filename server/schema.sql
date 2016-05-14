5e90cbdee75f2c9ddf6ef0e8632a521f10ed389a
d-vu <daniel.h.vu@gmail.com>
2016-05-13 17:03:13 -0700

    Created mySql tables.

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,40 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+DROP TABLE IF EXISTS `User`;
+    
+CREATE TABLE `user` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `userName` VARCHAR NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+
+DROP TABLE IF EXISTS `Room`;
+    
+CREATE TABLE `room` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomName` VARCHAR NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `userID` INTEGER NULL DEFAULT NULL,
+  `roomID` INTEGER NULL DEFAULT NULL,
+  `message` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (userID) REFERENCES `User` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (roomID) REFERENCES `Room` (`id`);
+
+
 
 /* Create other tables and define schemas for them here! */
 
